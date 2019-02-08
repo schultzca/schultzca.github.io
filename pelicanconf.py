9ab48071f574d0ef218aa31398ea2f9bc8be8ca4
Charles Alexander Schultz <schultz.ca@gmail.com>
2019-02-07 23:09:01 -0500

    Initial Pelican website with theme included.

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Charles Schultz'
+SITENAME = 'SchuTech'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+THEME = "themes/nmnlist"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
